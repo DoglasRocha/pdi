2a61401c5abbd2273b2d652a4b923249417a66d4
Doglas <doglas_rocha@protonmail.ch>
2024-10-20 01:15:16 -0300

    feat: implementa testes com todos os tamanhos de janela

--- trabalho2/blur.py
+++ trabalho2/blur.py
@@ -8,9 +8,11 @@ import typing
 
 
 def measure_runtime(func: typing.Callable) -> typing.Callable:
-    def wrapper(img: cv2.typing.MatLike, width: int, height: int) -> typing.Any:
+    def wrapper(
+        img: cv2.typing.MatLike, width: int, height: int, filler=None
+    ) -> typing.Any:
         t1 = time()
-        result = func(img, width, height)
+        result = func(img, width, height, filler)
         print(f"Tempo de execução da função {func.__name__}: {time() - t1}s")
         return result
 
@@ -19,14 +21,14 @@ def measure_runtime(func: typing.Callable) -> typing.Callable:
 
 @measure_runtime
 def blur_naive_algorithm(
-    img: cv2.typing.MatLike, width: int, height: int
+    img: cv2.typing.MatLike, width: int, height: int, filler=None
 ) -> cv2.typing.MatLike:
     window_size = width * height
 
     img_out = img.copy()
-    for row in range(height // 2, len(img) - height // 2):
-        for col in range(width // 2, len(img[row]) - width // 2):
-            for channel in range(len(img[row][col])):
+    for row in range(height // 2, img.shape[0] - height // 2):
+        for col in range(width // 2, img.shape[1] - width // 2):
+            for channel in range(img.shape[2]):
                 sum_ = 0
                 for window_row in range(row - height // 2, row + height // 2 + 1):
                     for window_col in range(col - width // 2, col + width // 2 + 1):
@@ -40,20 +42,20 @@ def blur_naive_algorithm(
 
 @measure_runtime
 def blur_separable_filter(
-    img: cv2.typing.MatLike, width: int, height: int
+    img: cv2.typing.MatLike, width: int, height: int, filler=None
 ) -> cv2.typing.MatLike:
     img_step_1 = img.copy()
 
     # horizontal
-    for row in range(height // 2, len(img) - height // 2):
-        for channel in range(len(img[row][0])):
+    for row in range(0, img.shape[0]):
+        for channel in range(img.shape[2]):
             sum_ = 0
             # first sum
             for window_col in range(0, width):
                 sum_ += img[row][window_col][channel]
             img_step_1[row][width // 2][channel] = sum_ / width
 
-            for window_col in range(width, len(img[row])):
+            for window_col in range(width, img.shape[1]):
                 sum_ += (
                     -img[row][window_col - width][channel]
                     + img[row][window_col][channel]
@@ -62,15 +64,15 @@ def blur_separable_filter(
 
     # vertical
     img_out = img_step_1.copy()
-    for col in range(width // 2, len(img) - width // 2):
-        for channel in range(len(img[0][col])):
+    for col in range(img.shape[1]):
+        for channel in range(img.shape[2]):
             sum_ = 0
             # first sum
             for window_row in range(0, height):
                 sum_ += img_step_1[window_row][col][channel]
             img_out[height // 2][col][channel] = sum_ / height
 
-            for window_row in range(height, len(img)):
+            for window_row in range(height, img.shape[0]):
                 sum_ += (
                     -img_step_1[window_row - height][col][channel]
                     + img_step_1[window_row][col][channel]
@@ -107,8 +109,10 @@ def blur_integral_image(
     img: cv2.typing.MatLike,
     width: int,
     height: int,
+    integral_image: cv2.typing.MatLike | None = None,
 ) -> cv2.typing.MatLike:
-    integral_image = build_integral_image(img)
+    if type(integral_image) == type(None):
+        integral_image = build_integral_image(img)
     img_out = img.copy()
 
     W, H = width // 2, height // 2
@@ -121,10 +125,10 @@ def blur_integral_image(
                 l, r = max(0, col - W - 1), min(col + W, img.shape[1] - 1)
                 value = (
                     integral_image[b][r][channel]
-                    - integral_image[t][r][channel]
-                    - integral_image[b][l][channel]
-                    + integral_image[t][l][channel]
-                ) / ((b - t) * (r - l))
+                    - (integral_image[t][r][channel] if b - t > 0 else 0)
+                    - (integral_image[b][l][channel] if r - l > 0 else 0)
+                    + (integral_image[t][l][channel] if r - l > 0 and b - t > 0 else 0)
+                ) / ((b - t if b - t > 0 else 1) * (r - l if r - l > 0 else 1))
                 img_out[row][col][channel] = value
 
     return img_out
--- trabalho2/main.py
+++ trabalho2/main.py
@@ -56,5 +56,55 @@ def main():
     cv2.destroyAllWindows()
 
 
+def test():
+    PATH_IMG_1 = "imagens/a01 - Original.bmp"
+    PATH_IMG_2 = "imagens/b01 - Original.bmp"
+
+    # teste imagem 1, todos os tamanhos de janela
+    print("======================== TESTE IMAGEM 1 ========================")
+    window_sizes = [(3, 3), (3, 13), (11, 1), (51, 21)]
+    img = open_image(PATH_IMG_1)
+    img_integral = blur.build_integral_image(img)
+    for index, w_size in enumerate(window_sizes):
+        print(f"======== TAMANHO DE JANELA: {w_size} ========")
+        img_ingenua = blur.blur_naive_algorithm(img, *w_size)
+        img_separavel = blur.blur_separable_filter(img, *w_size)
+        img_c_integral = blur.blur_integral_image(img, *w_size, img_integral)
+
+        cv2.imwrite(
+            f"out/a0{index + 2} - Borrada {w_size} algoritmo ingênuo.png", img_ingenua * 255
+        )
+        cv2.imwrite(
+            f"out/a0{index + 2} - Borrada {w_size} algoritmo separável.png", img_separavel * 255
+        )
+        cv2.imwrite(
+            f"out/a0{index + 2} - Borrada {w_size} algoritmo com imagem integral.png",
+            img_c_integral * 255,
+        )
+
+    # teste imagem 2, todos os tamanhos de janela
+    print("\n\n======================== TESTE IMAGEM 2 ========================")
+    window_sizes = [(7, 7), (11, 15)]
+    img = open_image(PATH_IMG_2)
+    img_integral = blur.build_integral_image(img)
+    for index, w_size in enumerate(window_sizes):
+        print(f"======== TAMANHO DE JANELA: {w_size} ========")
+        img_ingenua = blur.blur_naive_algorithm(img, *w_size)
+        img_separavel = blur.blur_separable_filter(img, *w_size)
+        img_c_integral = blur.blur_integral_image(img, *w_size, img_integral)
+
+        cv2.imwrite(
+            f"out/b0{index + 2} - Borrada {w_size} algoritmo ingênuo.png", img_ingenua * 255
+        )
+        cv2.imwrite(
+            f"out/b0{index + 2} - Borrada {w_size} algoritmo separável.png", img_separavel * 255
+        )
+        cv2.imwrite(
+            f"out/b0{index + 2} - Borrada {w_size} algoritmo com imagem integral.png",
+            img_c_integral * 255,
+        )
+
+
 if __name__ == "__main__":
-    main()
+    test()
+    # main()
