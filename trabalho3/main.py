bfc60b74538e2e6b78ca47b57a4af34bbca3f48a
Doglas <doglas_rocha@protonmail.ch>
2024-11-02 22:19:23 -0300

    fix: substitui np.add por +, coloca clip

--- trabalho3/main.py
+++ trabalho3/main.py
@@ -3,11 +3,10 @@ import cv2, numpy as np
 IMG_PATH = "img/Wind Waker GC.bmp"
 THRESHOLD_LUMINANCE = 0.525
 
-IMG_PATH = "img/GT2.BMP"
-THRESHOLD_LUMINANCE = 0.5
+# IMG_PATH = "img/GT2.BMP"
+# THRESHOLD_LUMINANCE = 0.5
 
-img = cv2.imread(IMG_PATH)
-img = img.astype(np.float32) / 255.0
+img = cv2.imread(IMG_PATH).astype(np.float32) / 255.0
 img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
 
 mask = img.copy()
@@ -19,14 +18,15 @@ for row in range(img.shape[0]):
 # cv2.imshow("mask", mask)
 
 blurred_masks = []
-for sigma in [2, 4, 8, 16, 32]:
+for sigma in [2, 4, 8, 16]:
     blurred_masks.append(cv2.GaussianBlur(mask.copy(), (0, 0), sigma))
 
 final_mask = np.zeros(img.shape)
 for mask in blurred_masks:
-    final_mask = np.add(final_mask, mask)
+    final_mask += mask
 
-bloom_img = np.add(0.92 * img, 0.08 * final_mask)
+final_mask = np.clip(final_mask, 0, 1)
+bloom_img = 0.92 * img + 0.08 * final_mask
 cv2.imshow("original", img)
 cv2.imshow("bloom", bloom_img)
 cv2.waitKey()
