bbc99e0c3d60d2a33b29f4d246d943475fa3f836
Doglas <doglas_rocha@protonmail.ch>
2024-11-17 17:20:49 -0300

    feat: implementar função de erosão de 'anormalidades', cálculo de métricas

--- trabalho4/main.py
+++ trabalho4/main.py
@@ -9,48 +9,71 @@ from segmentation import *
 from img_utils import *
 from operations import *
 
+
+def calculate_metrics(components: list[dict]) -> "tuple[float, float, float, float]":
+    n_pixels_per_component = [component["n_pixels"] for component in components]
+    mean = np.mean(n_pixels_per_component)
+    std = np.std(n_pixels_per_component)
+    min_ = np.min(n_pixels_per_component)
+    max_ = np.max(n_pixels_per_component)
+
+    return mean, std, min_, max_
+
+
+def erode_abnormalities(
+    img: cv2.typing.MatLike,
+    components: list[dict],
+    threshold: float,
+    kernel: "tuple[int, int]",
+) -> cv2.typing.MatLike:
+
+    for c in components:
+        if c["n_pixels"] < threshold:
+            continue
+
+        component_area = img[c["T"] : c["B"], c["L"] : c["R"], :]
+        eroded_area = cv2.erode(component_area, np.ones(kernel))
+        eroded_area = reshape_image(eroded_area)
+        img[c["T"] : c["B"], c["L"] : c["R"], :] = eroded_area
+
+    return img
+
+
 def count_rice(img: cv2.typing.MatLike) -> cv2.typing.MatLike:
     img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    sharped_image = sharp_image(img)
-    # cv2.imshow("sharp", sharped_image)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
-    binarized_img = normalize_locally_and_binarize_image(sharped_image)
-    non_noisy_img = supress_image_noise(binarized_img, (5,5))
+    binarized_img = normalize_locally_and_binarize_image(img)
+    non_noisy_img = supress_image_noise(binarized_img, (5, 5))
 
     # labeling
-    components = label(non_noisy_img.copy(), 0, 0, 0)
-    
-    n_pixels_per_component = [component['n_pixels'] for component in components]
-    mean = np.mean(n_pixels_per_component)
-    std = np.std(n_pixels_per_component)
+    components = label(non_noisy_img)
+    mean, std, min_, max_ = calculate_metrics(components)
 
-    print(f"{mean=} {std=} {std/mean=}")
+    if std / mean > 0.2:
+        img_with_some_erosions = non_noisy_img.copy()
+        while max_ > std + mean:
+            img_with_some_erosions = erode_abnormalities(
+                img_with_some_erosions, components, std + mean, (3, 3)
+            )
+            img_with_some_erosions = supress_image_noise(img_with_some_erosions, (3, 3))
+            components = update_components(img_with_some_erosions, components)
+            mean, std, min_, max_ = calculate_metrics(components)
 
-    if (std / mean > 0.2):
-        eroded = cv2.erode(non_noisy_img, np.ones((3, 3)))
-        eroded = reshape_image(eroded)
+    cv2.imshow("", img_with_some_erosions)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
-        cv2.imshow("eroded", eroded)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-        print(non_noisy_img.mean())
-        print(eroded.mean())
-        components = label(eroded.copy(), 0, 0, 0)
-        n_pixels_per_component = [component['n_pixels'] for component in components]
-        mean = np.mean(n_pixels_per_component)
-        std = np.std(n_pixels_per_component)
-
-        print(f"{mean=} {std=} {std/mean=}")
-
+    components = label(img_with_some_erosions)
     print(f"{len(components)} componentes detectados.")
     for c in components:
-        cv2.rectangle(img_out, (c["L"], c["T"]), (c["R"], c["B"]), (0, 0, 1))
+        cv2.rectangle(img_out, (c["L"], c["T"]), (c["R"], c["B"]), (0, 0, 1), 2)
 
     return img_out
 
-assert len(sys.argv) >= 2, "Por favor, insira como argumento para o script um caminho para uma imagem"
+
+assert (
+    len(sys.argv) >= 2
+), "Por favor, insira como argumento para o script um caminho para uma imagem"
 img_path = sys.argv[1]
 assert os.path.exists(img_path), "Por favor, insira um caminho válido"
 
