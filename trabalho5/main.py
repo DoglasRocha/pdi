e8817e253bd630ce5a156755e135724a7ecc0748
DOGLAS FRANCO MAURER DA ROCHA <dfmrocha@mppr.mp.br>
2024-12-04 16:50:39 -0300

    feat: esforços iniciais do trab 5

---
+++ trabalho5/main.py
@@ -0,0 +1,53 @@
+# ===============================================================================
+# Autor: Doglas Franco Maurer da Rocha
+# Universidade Tecnológica Federal do Paraná
+# ===============================================================================
+
+# ATENÇÃO: ESTE É UM PROGRAMA DE LINHA DE COMANDO
+# MODO DE USO: python main.py endereço_da_imagem_com_verde endereço_da_imagem_a_ser_colada
+import sys, os, cv2, numpy as np
+
+
+def main() -> None:
+    assert (
+        len(sys.argv) >= 3
+    ), "\n\nPor favor, insira como argumento para o script um caminho para uma imagem.\n\tExemplo: python main.py 0.bmp img.png"
+    img_path = sys.argv[1]
+    stamp_img_path = sys.argv[2]
+    assert os.path.exists(img_path), "Por favor, insira um caminho válido para a imagem base"
+    assert os.path.exists(stamp_img_path), "Por favor, insira um caminho válido para a imagem \"carimbo\""
+    
+    img = cv2.imread(img_path).astype(np.float32) / 255
+    stamp_img = cv2.imread(stamp_img_path).astype(np.float32) / 255
+    
+    stamp_img = cv2.resize(stamp_img, img.shape[::-1][1:])
+    
+    mask = np.zeros(img.shape[:-1])
+    
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            b, g, r = img[row, col]
+            if g > b and g > r:
+                #print(g * 2 / (b + r))
+                mask[row,col] = g / (b + r)
+            else:
+                mask[row,col] = 0
+                
+    mask = cv2.normalize(mask, None, 0, 1, cv2.NORM_MINMAX)
+    
+    stamped_img = img.copy()
+    for row in range(stamped_img.shape[0]):
+        for col in range(stamped_img.shape[1]):
+            if mask[row, col] != 0:
+                #print(mask[row, col], stamp_img[row, col])
+                # stamped_img[row, col] = stamp_img[row, col]
+                stamped_img[row, col] = mask[row, col] * stamp_img[row, col]# + (1 - mask[row, col]) * img[row, col]
+    
+    cv2.imshow("mask", mask)
+    cv2.imshow("stamp", stamp_img)
+    cv2.imshow("result", stamped_img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+    main()
