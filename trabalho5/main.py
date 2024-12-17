dea7484625e2d5661c6c5c0576f7af874048f9d5
Doglas <doglas_rocha@protonmail.ch>
2024-12-17 14:27:07 -0300

    ta quase bom

--- trabalho5/main.py
+++ trabalho5/main.py
@@ -31,13 +31,13 @@ def main() -> None:
     for row in range(img.shape[0]):
         for col in range(img.shape[1]):
             b, g, r = img[row, col]
-            if g > b and g > r and g * 0.625 > (b + r) / 2:
+            if g > b and g > r and g * 0.666 > (b + r) / 2:
                 # print(b, g, r)
                 mask[row, col] = 1 - g
 
-    # mask = np.clip(mask, 0, 255)
-    mask = np.where(mask > 0.99, 1, mask)
-    mask = cv2.normalize(mask, None, 0, 1, cv2.NORM_MINMAX)
+    mask = np.where(mask > 0.80, 1.0, mask)
+    mask = cv2.normalize(mask, None, 0.0, 1.0, cv2.NORM_MINMAX)
+    mask = np.round(mask, 3)
 
     mask_and_img = np.zeros(img.shape)
     for row in range(img.shape[0]):
@@ -56,14 +56,18 @@ def main() -> None:
             elif mask[row, col] == 1:
                 stamped_img[row, col] = img[row, col]
             else:
+                o_b, _, o_r = img[row, col]
+                _, g, _ = stamp_img[row, col]
+                g *= mask[row, col]
                 stamped_img[row, col] = (1 - mask[row, col]) * stamp_img[
                     row, col
-                ] + mask[row, col] * np.array([img[row, col][0], 0, img[row, col][2]])
+                ] + mask[row, col] * np.array([o_b, 0, o_r])
 
     cv2.imshow("original", img)
     cv2.imshow("mask", mask)
     cv2.imshow("mask and img", mask_and_img)
     cv2.imshow("result", stamped_img)
+    # cv2.imwrite("aaaa.png", mask * 255)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
